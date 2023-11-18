aa0bb682378c50559cb5b907e0a704e75af6550d
Matvey Korotygin <matveykaklanter@mail.ru>
2023-11-18 09:28:55 +0100

    ~

--- p-4.js
+++ p-4.js
@@ -11,9 +11,11 @@ const getJsonMemoize = memoize(getData, 1000);
 (async function() {
     await sleep(3000);
     console.log(await getJsonMemoize()); // 1  
+    await sleep(1000);
     console.log(await getJsonMemoize()); // 1  
     await sleep(3000);
     console.log(await getJsonMemoize()); // 2  
+    await sleep(1000);
     console.log(await getJsonMemoize()); // 2  
 }())
 
@@ -31,3 +33,30 @@ function memoize(makeRequest, ms) {
         return cache[count];
     }
 }
+
+
+// advanced
+function memoize(func, ttl) {
+    const memoized = async function(...args) {
+      const cache = memoized.cache;
+      const timeoutId = memoized.timeoutId;
+   
+      if (timeoutId && cache.has(timeoutId)) {
+        return cache.get(timeoutId);
+      }
+  
+      const result = await func.apply(this, args);
+  
+      memoized.timeoutId = setTimeout(() => {
+        delete memoized.timeoutId;
+        cache.clear();
+      }, ttl);
+      memoized.cache = cache.set(memoized.timeoutId, result);
+  
+      return result;
+    }
+  
+    memoized.cache = new Map();
+    return memoized
+  }
+  
---
+++ p-5.js
@@ -0,0 +1,33 @@
+const sentence = 'my first sentece'
+
+// 1
+// function revertWords(sentence) {
+//     return sentence.split(' ').map(
+//         x => x.split('').reverse().join('')
+//     ).join(' ');
+// }
+
+// сделать
+// function revertWords(sentence) {
+//     let result = [];
+//     for (let i = 0; i < sentence.length; i++) {
+//         let word = '';
+//     }
+// }
+
+// console.log(revertWords(sentence));
+
+
+
+// for (let i = 0; i < 10; i++) {
+//     setTimeout(() => {
+//         console.log(i);
+//     }, 0);
+// }
+
+
+// дебаунс, тротл
+
+
+
+
